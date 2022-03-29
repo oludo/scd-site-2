db6bc18865166af4cd5f7f108c37e7dfc91f6f90
Oludo <lucas@oludo.com>
2022-03-29 15:39:03 -0300

    Redirect visitors to new site

--- components/base/Buttons.js
+++ components/base/Buttons.js
@@ -35,3 +35,25 @@ const ButtonWithIconContainer = styled(Button)`
   align-items: center;
   gap: 4px;
 `;
+
+export const ButtonLink = styled.a`
+  background-color: var(--button-bg);
+  border: none;
+  border-radius: 13px;
+  color: var(--button-color);
+  font-family: system-ui;
+  font-weight: 600;
+  cursor: pointer;
+  font-weight: 600;
+  min-width: 4ch;
+  padding: 0.8rem 1.6em;
+  position: relative;
+  text-align: center;
+  text-decoration: none;
+  transition: all 0.08s ease-in;
+
+  &:hover,
+  &:focus {
+    transform: scale(1.03);
+  }
+`
